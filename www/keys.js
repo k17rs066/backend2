f54e3b977757bb57776149baa3c8ef4c9f4b4274
k17rs066 <k17rs066@st.kyusan-u.ac.jp>
2018-12-10 13:44:19 +0900

    delete

--- www/keys.js
+++ www/keys.js
@@ -1,3 +1,3 @@
 // This is a JavaScript file
-var appKey = "";
-var clientKey = "";
+var appKey = "893a19de5e24baa07f7844c88dd6bf7844920ac6d29e8336c3d1f3c1008a6d88";
+var clientKey = "fc8b789290c4679c202bb6960d4f35f3253e2e774e628cb7b9ccb1c8bdbb5b16";
